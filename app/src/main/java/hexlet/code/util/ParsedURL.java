a1c9281b827fa5c1a736ad5fcaf9241c58bb41cc
Alex <alextetekin@gmail.com>
2023-11-27 06:09:36 +0300

    build: added method getListOfLastChecks()

--- app/src/main/java/hexlet/code/util/ParsedURL.java
+++ app/src/main/java/hexlet/code/util/ParsedURL.java
@@ -1,6 +1,13 @@
 package hexlet.code.util;
 
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.repository.UrlRepository;
+
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParsedURL {
     public static String getNormalizedURL(URL url) {
@@ -10,5 +17,23 @@ public class ParsedURL {
         String colonBeforePort = url.getPort() == -1 ? "" : ":";
         String port = url.getPort() == -1 ? "" : String.valueOf(url.getPort());
         return protocol + symbols + host + colonBeforePort + port;
+    }
+
+    public static Map<Long, UrlCheck> getListOfLastChecks() throws SQLException {
+        var urls = UrlRepository.getEntities();
+        Map<Long, UrlCheck> result = new HashMap<>();
+
+        for (var url: urls) {
+            var id = url.getId();
+            UrlCheck lastCheck;
+            if (UrlCheckRepository.getLastCheck(id).isPresent()) {
+                lastCheck = UrlCheckRepository.getLastCheck(id).get();
+            } else {
+                lastCheck = null;
+            }
+            result.put(id, lastCheck);
+        }
+
+        return result;
     }
 }
